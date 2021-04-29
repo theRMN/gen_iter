321d57d111c417f3d5b3b0ec4db3374007f34a9a
theRMN <roman.vassev@gmail.com>
2021-04-29 10:00:07 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import json
+from hashlib import md5
+
+
+class URLmaker:
+    def __init__(self, path, count: int):
+        self.path = path
+        self.count = count
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        b_url = 'https://en.wikipedia.org/wiki/'
+        with open(self.path) as f:
+            file = json.load(f)
+            try:
+                name = file[self.count]['name']['common'].replace(' ', '_').replace(',', '')
+                result = f'{name}: {b_url}{name}'
+                self.count += 1
+                with open('result.txt', 'a', encoding='utf-8') as r:
+                    r.write(result + '\n')
+            except IndexError:
+                raise StopIteration
+        return result
+
+
+for i in URLmaker('countries.json', 0):
+    print(i)
+
+
+def line_hash(path):
+    with open(path, encoding='utf-8') as f:
+        for line in f:
+            yield md5(line.replace('\n', '').encode()).hexdigest()
+
+
+for i in line_hash('result.txt'):
+    print(i)
